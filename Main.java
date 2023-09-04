efd1f2201c3f47b806ded77b26d0d417f5cee9fd
haedanui <rhs9705kr@naver.com>
2023-09-04 18:31:01 +0900

    메인 파일 추가

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String[] args) {
-        
+        int x = 10;
+        int y = 10;
+        Add add = new Add();
+        add.add(x, y);
+        sub.sub(x, y);
+        Mul.mul(x, y);
+        div.div(x, y);
     }
 }
